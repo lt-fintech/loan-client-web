7de4d4132d2866236d6f1ed29ba1288ab3a9dfc9
zhonghcc <zhonghcc@gmail.com>
2020-09-01 13:47:13 +0800

    add vant

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,7 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <van-button type="primary">主要按钮</van-button>
   </div>
 </template>
 
@@ -12,7 +13,7 @@ import HelloWorld from '@/components/HelloWorld.vue'
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    HelloWorld,
   }
 }
 </script>
