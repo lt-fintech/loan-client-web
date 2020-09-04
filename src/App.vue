c6a3f1fdc434076e88dee4b30c66d6cb5f1c510a
zhonghcc <zhonghcc@gmail.com>
2020-09-04 13:15:24 +0800

    box

--- src/App.vue
+++ src/App.vue
@@ -9,11 +9,39 @@
 </template>
 
 <style>
+*{margin:0;padding:0}
+
+li{list-style:none}
+
+img{vertical-align:top;border:none}
+
+body {
+  text-align:center;
+  background:#ccc;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
+}
+  #app{
+    text-align: left;
+    margin:auto;
+    width:800px;
+    margin-top:20px;
+    margin-bottom:20px;
+    box-shadow:2px 2px 5px #4c5e70;
+    background-color:#ffffff;
+    min-height:1000px;
+    border-radius: 5px;
+  }
+
+@media screen and (max-width:1000px){
+  #app{
+    margin-top:0;
+    width:100%;
+  }
 }
 
 #nav {
--- src/views/Trail.vue
+++ src/views/Trail.vue
@@ -1,6 +1,16 @@
 <template>
   <div class="trail">
+  <van-row>
+    <van-col span="8">
     <van-button type="primary">主要按钮</van-button>
+    </van-col>
+    <van-col span="8">
+    <van-button type="primary">按钮</van-button>
+    </van-col>
+    <van-col span="8">
+    <van-button type="primary">按钮</van-button>
+    </van-col>
+  </van-row>
   </div>
 </template>
 
