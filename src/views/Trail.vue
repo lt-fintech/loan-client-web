c582aa7d22ced885f3f601d2fb406c6e561f3bd3
zhonghcc <zhonghcc@gmail.com>
2020-09-27 19:41:38 +0800

    request date

--- src/views/Trail.vue
+++ src/views/Trail.vue
@@ -6,7 +6,7 @@
         <van-cell-group>
       <van-field v-model="amount" type="digit" label="借款金额"></van-field>
       <van-field v-model="repayDay" type="digit" label="还款日"></van-field>
-      <van-field v-model="paymentDate" type="digit" label="借款日"></van-field>
+      <van-cell title="借款日" :value="requestFormatDate" label="借款日" @click="calendarShow = true"></van-cell>
         </van-cell-group>
       </van-form>
     </van-row>
@@ -15,9 +15,25 @@
     <van-button type="primary" class="submit" block @click="trail">贷款试算</van-button>
     </van-cell-group>
   </van-row>
-  <van-row>
-    {{repayplan}}
+  <van-row v-if="!loading">
+    <van-cell-group>
+      <van-cell title="借款金额" :value="amount"></van-cell>
+      <van-cell title="每月还款日" :value="trialdata.RepayDay"></van-cell>
+      <van-cell title="总期数" :value="trialdata.TermNum"></van-cell>
+      <van-cell title="还款金额" size="large" :value="trialdata.Prin+trialdata.Interest" :label="'本金:'+trialdata.Prin+' 利息:'+trialdata.Interest"></van-cell>
+    </van-cell-group>
   </van-row>
+  <van-row v-if="!loading">
+    <h2 class="title">还款计划</h2>
+    <van-steps direction="vertical" active="-1">
+<van-step v-for="item in trialdata.RepayPlans" v-bind:key="item.TermNum">
+    <h3>还款金额：{{item.Prin+item.Interest}}</h3>
+    <p>还款日:{{dateFormatter(new Date(item.RepayDate))}}</p>
+    <p>本金:{{item.Prin}} 利息:{{item.Interest}}</p>
+  </van-step>
+</van-steps>
+  </van-row>
+  <van-calendar v-model="calendarShow" @confirm="calendarConfirm" />
   </div>
 </template>
 
@@ -29,10 +45,14 @@ export default {
   props:[],
   data(){
     return {
-      repayplan:{},
+      trialdata:null,
       amount:null,
       repayDay:null,
       paymentDate:null,
+      requestFormatDate: null,
+      requestDate:null,
+      loading:true,
+      calendarShow:false
     }
   },
   created(){
@@ -40,21 +60,34 @@ export default {
   },
   methods:{
     trail(){
+      this.loading = true;
       console.log(this.amount);
       var data = {
         "amount": 500,
         "rate": 500,
-        "requestTime": 1597640186380,
+        "requestFormatDate": null,
+        "requestDate":null,
         "repayDay": 17,
         "termNum": 6,
         "interestType": "EQUAL_LOAN"
       };
       axios.post("http://loanapi.zhonghcc.com/loan/trail",JSON.stringify(data)).then(resp=>{
-        this.repayplan = resp.data;
+        this.trialdata = resp.data;
+        this.loading = false;
       }).catch(err=>{
         console.log(err);
         Notify({ type: 'warning', message: '运行失败，请联系客服处理' });
       });
+    },
+    dateFormatter (date) {
+      const month = (date.getMonth() + 1).toString().padStart(2, '0');
+      const strDate = date.getDate().toString().padStart(2, '0');
+      return `${date.getFullYear()}-${month}-${strDate}`;
+    },
+    calendarConfirm(date){
+      this.calendarConfirm=false;
+      this.requestDate = date;
+      this.requestFormatDate = this.dateFormatter(date);
     }
   },
   components: {
